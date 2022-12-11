5b338c5b8a70adf6aaa7beef7732da3ac043f1b6
al-mahfuz <almahfuzhossen@gmail.com>
2022-12-11 13:35:19 +0600

    first commit

---
+++ basic.js
@@ -0,0 +1,55 @@
+// variable
+var deposit = 400;
+// condition
+if (deposit > 600){
+
+}
+else if(deposit < 200){
+    
+}
+else if(deposit == 200){
+
+}
+else if(deposit != 200){
+
+}
+else if(deposit <= 200){
+
+}
+else if(deposit >= 200){
+
+}
+else{
+
+}
+
+// array
+const numbers =[45, 543, 45, 12, 3, 23];
+const numberCount = numbers.length;
+numberCount.pop();
+number.push(111);
+number[2] = 555;
+// check whether 222 included in array..
+if (numbers.indexOf(222) != -1){
+
+}
+if (numbers.includes(222)){
+
+}
+
+// loop
+// while, for
+for (const number of numbers){
+
+}
+
+// function
+
+function fullName(first, second){
+    const name = first + ' '+ second;
+}
+const person = fullName('Rahim', 'Ali');
+
+// object
+
+const bottle = {color: 'yellow', contain: 'water', price : 50}
