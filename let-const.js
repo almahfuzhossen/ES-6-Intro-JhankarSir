f592671594eeb212067c6651d64f7b31654472b8
al-mahfuz <almahfuzhossen@gmail.com>
2022-12-11 17:20:54 +0600

    let const

---
+++ let-const.js
@@ -0,0 +1,24 @@
+// break up with var
+let balance = 1240;
+balance = 1240;
+
+const userName = 'janPkahi Potas Potas';
+const weTogether = 'ami' + userName;
+console.log(weTogether);
+// userName = 'moyna pakhi';
+
+const numbers =[34,445,56,656,3];
+// numbers = [23,556,67] not allowed
+number.push(22);
+number[1] = 333;
+
+let sum = 0;
+for (let i = 0; i < number.length; i++){
+    const number = numbers[i];
+    console.log(number);
+    sum = sum + number;
+}
+
+const student ={roll : 222, name : 'mofij', job : 'intern'};
+student.name = 'mofazzol';
+// student = {name : mofazzol}gi
